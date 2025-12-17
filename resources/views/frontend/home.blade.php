b3d9255bc3a9242fab067d686f9fbdc4f30fa421
unknown <choudharyamjad04@gmail.com>
2025-12-17 19:08:34 +0500

    Home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -200,9 +200,9 @@
       @if(isset($totalCategories) && $totalCategories > 7)
         <div class="flex justify-center mt-12">
           <a href="{{ route('frontend.categories') }}"
-            class="inline-flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 transition duration-150 ease-in-out">
+            class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 transition duration-150 ease-in-out">
             Show More
-            <svg class="ml-2 -mr-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+            <svg class="ml-2 -mr-1 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd"
                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                 clip-rule="evenodd" />
